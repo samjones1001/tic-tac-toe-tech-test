d5c6ea63b98dff63de0233061fc779a2a26d0952
Sam Jones <sam.jones494@googlemail.com>
2017-03-10 09:19:14 +0000

    game creates two players at initialization

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,8 @@
 class Game
+
+  def initialize(player_1 = Player.new('Sam'), player_2 = Player.new('Dave'))
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,16 @@
 require 'game'
 
 describe 'Game' do
-  
+
+  subject(:game)      { described_class.new }
+  let(:player_1)      { double(:player) }
+  let(:player_2)      { double(:player) }
+
+  describe '#initialize' do
+    it 'creates two new players' do
+      expect(Player).to receive(:new).and_return(:player_1)
+      expect(Player).to receive(:new).and_return(:player_2)
+      new_game = Game.new
+    end
+  end
 end

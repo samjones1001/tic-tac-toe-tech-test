d87df6ea636af848e4faf452c5a893916e219833
Sam Jones <sam.jones494@googlemail.com>
2017-03-10 14:25:53 +0000

    added extra tests to square'

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -3,10 +3,23 @@ require 'square'
 describe Square do
 
   subject(:square)      { described_class.new }
+  let(:player)          { double(:player) }
 
    describe '#initialize' do
      it 'is not taken at initialization' do
-       expect(square.taken).to eq(false)
+       expect(square.taken).to be(false)
+     end
+   end
+
+   describe '#set_to_taken' do
+     it 'sets the square\'s taken attribute to true' do
+       square.set_to_taken(player)
+       expect(square.taken).to be(true)
+     end
+
+     it 'knows which player has taken the square' do
+       square.set_to_taken(player)
+       expect(square.taken_by).to eq(player)
      end
    end
 end

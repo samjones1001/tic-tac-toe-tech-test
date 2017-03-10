d5789b73d474bcf1e194153526ff2f395b988359
Sam Jones <sam.jones494@googlemail.com>
2017-03-10 13:02:27 +0000

    Board initializes with a grid of 9 squares

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,13 @@
 class Board
 
-  
+  attr_reader :grid
+
+  def initialize(square = Square)
+    @grid = {}
+    [*'A'..'C'].each do |letter|
+      [*1..3].each do |number|
+        grid["#{letter}#{number}".to_sym] = square.new
+      end
+    end
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,7 +3,7 @@ require 'board'
 describe Board do
   subject(:board)     { described_class.new }
 
-  xdescribe '#initialize' do
+  describe '#initialize' do
     it 'creates a grid of nine squares' do
       expect(board.grid.length).to eq(9)
     end

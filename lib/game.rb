a186c9fe15ee594fbd40c6188c87fadc18bc36f1
Sam Jones <sam.jones494@googlemail.com>
2017-03-10 09:30:19 +0000

    players default with the names X and O

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
 class Game
 
-  def initialize(player_1 = Player.new('Sam'), player_2 = Player.new('Dave'))
+  attr_reader :player_1, :player_2
+
+  def initialize(player_1 = Player.new('X'), player_2 = Player.new('O'))
     @player_1 = player_1
     @player_2 = player_2
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,16 +1,21 @@
 require 'game'
 
-describe 'Game' do
+describe Game do
 
   subject(:game)      { described_class.new }
-  let(:player_1)      { double(:player) }
-  let(:player_2)      { double(:player) }
+  let(:player_1)      { double(:player_1) }
+  let(:player_2)      { double(:player_2) }
 
   describe '#initialize' do
     it 'creates two new players' do
       expect(Player).to receive(:new).and_return(:player_1)
       expect(Player).to receive(:new).and_return(:player_2)
       new_game = Game.new
+    end
+
+    it 'by default creates players with the names X and O' do
+      expect(game.player_1.name).to eq('X')
+      expect(game.player_2.name).to eq('O')
     end
   end
 end
